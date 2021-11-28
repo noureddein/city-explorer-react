1cf1c0afe0b2bf042c61cd08e4c121cca374d745
noureddein <noureddein@gmail.com>
2021-11-28 18:30:00 +0200

    finish lab 6

--- src/components/App.js
+++ src/components/App.js
@@ -10,11 +10,13 @@ class App extends React.Component {
         super()
         this.state = {
             cityInfo: '',
-
+            errMsg: false,
+            disErrMsg: '',
         }
         this.handleForm = this.handleForm.bind(this)
+        this.resetHandler = this.resetHandler.bind(this)
     }
-    handleForm(event) {
+    async handleForm(event) {
         event.preventDefault()
         const q = event.target.userInput.value
         const key = process.env.REACT_APP_ACCESS_TOKEN
@@ -22,23 +24,46 @@ class App extends React.Component {
         const URL = `https://eu1.locationiq.com/v1/search.php?q=${q}&key=${key}&format=${format}&limit=1`
         axios.get(URL)
             .then(res => {
-                const results = res.data[0]
+                if (res.status === 200) {
+                    const results = res.data[0]
+                    this.setState({
+                        errMsg: false,
+                        cityInfo: {
+                            lon: results.lon,
+                            lat: results.lat,
+                            display_name: results.display_name,
+                        }
+                    })
+                }
+            })
+            .catch(err => {
                 this.setState({
-                    cityInfo: {
-                        lon: results.lon,
-                        lat: results.lat,
-                        display_name: results.display_name,
-                    }
+                    cityInfo: '',
+                    errMsg: true,
+                    disErrMsg: `Bad Request ${err.status}`
                 })
-
-            }).catch(error => console.log(error))
-
-
-
+            })
     }
+
+    resetHandler() {
+        this.setState({
+            cityInfo: '',
+            errMsg: false,
+            disErrMsg: '',
+        })
+    }
+
+
     render() {
         return (
             <React.Fragment>
+                <header>
+                    <nav>
+                        <p>
+                            City Explorer
+                        </p>
+                    </nav>
+                </header>
                 <div className="formContainer">
                     <Form onSubmit={this.handleForm}>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
@@ -50,9 +75,11 @@ class App extends React.Component {
                         </Form.Group>
 
                         <Button variant="primary" type="submit">Explore!</Button>
+                        <Button variant="primary" type="button" onClick={this.resetHandler} className="m-1">Clear</Button>
                     </Form>
-
-                    <CardText cityInfo={this.state.cityInfo && this.state.cityInfo}
+                    <CardText
+                        cityInfo={this.state.cityInfo && this.state.cityInfo}
+                        errMsg={this.state.errMsg && this.state.disErrMsg}
                     />
                 </div>
 
--- src/components/CardText.js
+++ src/components/CardText.js
@@ -4,29 +4,38 @@ class CardText extends React.Component {
 
     render() {
         return (
-            <>{
-                this.props.cityInfo ?
-                    <div><Card className="card">
-                        <Card.Body>City Name: {this.props.cityInfo.display_name}</Card.Body>
+            <>
+                {console.log(this.props.cityInfo)}
+                {
+                    this.props.cityInfo ?
+                        <div><Card className="card">
+                            <Card.Body>City Name: {this.props.cityInfo.display_name}</Card.Body>
+                        </Card>
+                            <Card className="card">
+                                <Card.Body>Latitude: {this.props.cityInfo.lat}</Card.Body>
+                            </Card>
+                            <Card className="card">
+                                <Card.Body>Longitude: {this.props.cityInfo.lon}</Card.Body>
+                            </Card>
+                            <Container>
+                                <Row>
+                                    <Col xs={10} md={10} style={{ margin: 'auto' }}>
+                                        <Image
+                                            src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_ACCESS_TOKEN}&center=${this.props.cityInfo.lat},${this.props.cityInfo.lon}&zoom=10`}
+                                            thumbnail />
+                                    </Col>
+                                </Row>
+                            </Container></div>
+                        :
+                        ''
+                }
+
+                {
+                    this.props.errMsg ? <Card className="card">
+                        <Card.Body>Error: {this.props.errMsg}</Card.Body>
                     </Card>
-                        <Card className="card">
-                            <Card.Body>Latitude: {this.props.cityInfo.lat}</Card.Body>
-                        </Card>
-                        <Card className="card">
-                            <Card.Body>Longitude: {this.props.cityInfo.lon}</Card.Body>
-                        </Card>
-                        <Container>
-                            <Row>
-                                <Col xs={10} md={10} style={{ margin: 'auto' }}>
-                                    <Image
-                                        src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_ACCESS_TOKEN}&center=${this.props.cityInfo.lat},${this.props.cityInfo.lon}&zoom=10`}
-                                        thumbnail />
-                                </Col>
-                            </Row>
-                        </Container></div>
-                    :
-                    ''
-            }
+                        : ''
+                }
 
 
 
