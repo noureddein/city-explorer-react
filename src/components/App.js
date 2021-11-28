9796da99ef22a53d8a74821aa5f867a0393407c7
noureddein <noureddein@gmail.com>
2021-11-28 13:29:07 +0200

    Ready to start

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1> Hello </h1>
+      </React.Fragment>
+    )
+  }
+}
+
+export default App;
